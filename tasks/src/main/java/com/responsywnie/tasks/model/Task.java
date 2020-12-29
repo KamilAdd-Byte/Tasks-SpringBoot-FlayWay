fdad8f87d6105b8cf855276649d59f21e76f505a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-12-29 23:25:33 +0100

    Set strategy GenerationType to IDENTITY for Task and TaskGroup

--- tasks/src/main/java/com/responsywnie/tasks/model/Task.java
+++ tasks/src/main/java/com/responsywnie/tasks/model/Task.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-public class Task {
+public class Task{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -15,54 +15,50 @@ public class Task {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    private LocalDateTime createdOn;
-    private LocalDateTime updatedOn;
+    //new Audit() wywołanie
+    @Embedded
+    private Audit audit = new Audit();
+    @ManyToOne
+    @JoinColumn(name = "task_group_id")
+    private TaskGroup group;
 
     public Task() {
     }
-
     private LocalDateTime getDeadline() {
         return deadline;
     }
-
     private void setDeadline(LocalDateTime deadline) {
         this.deadline = deadline;
     }
-
     public int getId() {
         return id;
     }
-
     private void setId(int id) {
         this.id = id;
     }
-
     private String getDescription() {
         return description;
     }
-
     private void setDescription(String description) {
         this.description = description;
     }
-
     public boolean isDone() {
         return done;
     }
-
     public void setDone(boolean done) {
         this.done = done;
+    }
+    private TaskGroup getGroups() {
+        return group;
+    }
+    private void setGroups(TaskGroup groups) {
+        this.group = groups;
     }
     public void updateForm(final Task source){
         description = source.description;
         done = source.done;
         deadline = source.deadline;
+        group = source.group;
     }
-    @PrePersist
-    void prePersist(){
-        createdOn = LocalDateTime.now();
-    }
-    @PreUpdate
-    void preMerge(){
-        updatedOn = LocalDateTime.now();
-    }
+
 }
