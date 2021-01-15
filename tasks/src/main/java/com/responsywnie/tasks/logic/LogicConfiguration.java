1685d7c3a01a2a23b220c524d4f1b024d411067a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-15 17:34:59 +0100

    Learn bean xml

--- tasks/src/main/java/com/responsywnie/tasks/logic/LogicConfiguration.java
+++ tasks/src/main/java/com/responsywnie/tasks/logic/LogicConfiguration.java
@@ -3,8 +3,10 @@ package com.responsywnie.tasks.logic;
 import com.responsywnie.tasks.config.TasksConfigurationProperties;
 import com.responsywnie.tasks.repositories.ProjectRepository;
 import com.responsywnie.tasks.repositories.TaskGroupRepository;
+import com.responsywnie.tasks.repositories.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 public class LogicConfiguration {
@@ -16,5 +18,12 @@ public class LogicConfiguration {
             final TasksConfigurationProperties config
     ){
         return new ProjectService(projectRepository,taskGroupRepository,config);
+    }
+    @Bean
+    TaskGroupService taskGroupService(
+            final TaskGroupRepository taskGroupRepository,
+            final TaskRepository taskRepository
+            ){
+        return new TaskGroupService(taskGroupRepository,taskRepository);
     }
 }
---
+++ tasks/src/test/java/com/responsywnie/tasks/model/TestConfiguration.java
@@ -0,0 +1,56 @@
+package com.responsywnie.tasks.model;
+
+import com.responsywnie.tasks.repositories.TaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.*;
+
+@Configuration
+public class TestConfiguration {
+
+
+    @Profile({"integration","!prod"})
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+            private Map<Integer, Task> taskMap = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(taskMap.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return false;
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(taskMap.get(id));
+            }
+
+            @Override
+            public Task save(Task entity) {
+                return taskMap.put(taskMap.size() + 1, entity);
+            }
+        };
+    }
+}
