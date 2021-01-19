915da4088ae8f51392239ac8887cb1a15a7bb955
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-19 19:13:03 +0100

    Done e2e test add task

--- tasks/src/test/java/com/responsywnie/tasks/controller/TaskControllerE2ETest.java
+++ tasks/src/test/java/com/responsywnie/tasks/controller/TaskControllerE2ETest.java
@@ -1,9 +1,19 @@
 package com.responsywnie.tasks.controller;
 
+import com.responsywnie.tasks.model.Task;
+import com.responsywnie.tasks.repositories.TaskRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TaskControllerE2ETest {
@@ -13,4 +23,21 @@ class TaskControllerE2ETest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    TaskRepository repo;
+
+    @Test
+    void httpGet_returnsAllTasks(){
+        //given
+        int initial = repo.findAll().size();
+        repo.save(new Task("foo", LocalDateTime.now()));
+        repo.save(new Task("bar", LocalDateTime.now()));
+
+        //when
+        Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks",Task[].class);
+
+        //then
+        assertThat(result).hasSize(initial+2);
+    }
 }
