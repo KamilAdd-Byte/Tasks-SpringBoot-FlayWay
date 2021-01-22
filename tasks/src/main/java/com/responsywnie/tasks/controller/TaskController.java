7a44da0d02078ad16a88d5c4794db2e06719270d
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-22 20:04:01 +0100

    Attempt add parametr to readDoneTask method

--- tasks/src/main/java/com/responsywnie/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/responsywnie/tasks/controller/TaskController.java
@@ -5,7 +5,6 @@ import com.responsywnie.tasks.repositories.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
