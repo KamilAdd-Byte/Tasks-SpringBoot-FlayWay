99fcf488e5a2254aee20b1cbfdc8b697155e5f3f
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-22 19:59:35 +0100

    Add RequestMapping in TaskController and create method search done witch tasks

--- tasks/src/main/java/com/responsywnie/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/responsywnie/tasks/controller/TaskController.java
@@ -4,6 +4,8 @@ import com.responsywnie.tasks.model.Task;
 import com.responsywnie.tasks.repositories.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
@@ -12,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
@@ -22,29 +25,36 @@ public class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @GetMapping(params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the task!");
         return ResponseEntity.ok(repository.findAll());
     }
-//    @GetMapping(value = "/tasks")
-//    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
-//        logger.info("Custom pageable");
-//        return ResponseEntity.ok(repository.findAll(page).getContent());
-//    }
-    @GetMapping("/tasks/{id}")
+    @GetMapping
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+    @GetMapping("/{id}")
     ResponseEntity<Task> readTask(@PathVariable int id){
         return repository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-    @PostMapping("/tasks")
+    @GetMapping(value = "/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state){
+        return ResponseEntity.ok(
+                repository.findByDone(state)
+        );
+    }
+
+    @PostMapping
     ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate){
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
         if (!repository.existsById(id)){
             return ResponseEntity.notFound().build();
@@ -57,7 +67,7 @@ public class TaskController {
         return ResponseEntity.noContent().build();
     }
     @Transactional
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable int id){
         if (!repository.existsById(id)){
             return ResponseEntity.notFound().build();
