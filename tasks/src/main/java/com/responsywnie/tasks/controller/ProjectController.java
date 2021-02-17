4adcc91b9d0c57cd8ce238e29cffa8daf5f96fc4
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-17 19:18:43 +0100

    Add projectStep method to project controller

--- tasks/src/main/java/com/responsywnie/tasks/controller/ProjectController.java
+++ tasks/src/main/java/com/responsywnie/tasks/controller/ProjectController.java
@@ -1,17 +1,42 @@
 package com.responsywnie.tasks.controller;
 
+import com.responsywnie.tasks.logic.ProjectService;
+import com.responsywnie.tasks.model.ProjectStep;
 import com.responsywnie.tasks.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
     @GetMapping
-    String showProjects(Model model){
+    String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+        projectService.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt");
+        return "projects";
+
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
